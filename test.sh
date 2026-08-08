09d5dbd796cc24deb9fcdea94ba373fbae27ed67
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-08-08 14:38:07 +0300

    Sat Aug  8 14:38:07 EEST 2026

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ pip install -r requirements.txt
 if test -z $HF_TOKEN; then
 	echo "exportati HF_TOKEN"
 else
-	whispermlx "$AUDIO_FILE" --device $DEVICE --diarize --hf_token $HF_TOKEN --language en --model large-v3 --output_dir $odir --output_format txt
+	whispermlx "$AUDIO_FILE" --device $DEVICE --language en --model large-v3 --output_dir $odir --output_format txt
 	ofile=$odir/$(basename $AUDIO_FILE | sed -e "s/\.m4a//g").txt
 	echo "=== Transcription"
 	cat $ofile
