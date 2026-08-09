51d2d98e6a7081c6d7108f0fb7cc84a321c87017
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-08-09 22:45:18 +0300

    Sun Aug  9 22:45:18 EEST 2026

--- test.sh
+++ test.sh
@@ -25,6 +25,7 @@ else
 	whispermlx "$AUDIO_FILE" --compression_ratio_threshold 2.4 --condition_on_previous_text False --device $DEVICE --diarize --hf_token $HF_TOKEN --language ro --logprob_threshold -1.0 --model large-v3 --no_speech_threshold 0.6 --output_dir $odir --output_format json
 	ofile=$odir/$(basename $AUDIO_FILE | sed -e "s/\.m4a//g").json
 	echo "=== Transcription"
+	jq . $ofile
 	cat $ofile
 	echo "==="
 fi
